222fe7f7fec16a5035fc7a79379b37c5a73da80f
Avanegasf93 <112149548+Avanegasf93@users.noreply.github.com>
2022-10-24 02:58:44 -0500

    Commit 1
    Commit 1

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,38 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    dataset=pd.read_fwf("clusters_report.txt", widths = [9, 16, 16, 80], skiprows = [0,1,2,3],
+    names = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'], 
+    na_filter = False)
 
-    return df
+    dataset['principales_palabras_clave'].replace({"  ": " "}, regex=True, inplace=(True))
+    dataset['principales_palabras_clave'].replace({"   ": " "}, regex=True, inplace=(True))
+    dataset['principales_palabras_clave'].replace({"    ": " "}, regex=True, inplace=(True))
+    dataset['principales_palabras_clave'].replace({"     ": " "}, regex=True, inplace=(True))
+
+    for indice, contenido in enumerate(dataset['cluster']):
+        if contenido =='':
+            dataset['cluster'].iloc[indice] = dataset['cluster'].iloc[indice-1]
+
+    for indice, contenido in enumerate(dataset['cantidad_de_palabras_clave']):
+        if contenido =='':
+            dataset['cantidad_de_palabras_clave'].iloc[indice] = dataset['cantidad_de_palabras_clave'].iloc[indice-1]
+
+    for indice, contenido in enumerate(dataset['porcentaje_de_palabras_clave']):
+        if contenido =='':
+            dataset['porcentaje_de_palabras_clave'].iloc[indice] = dataset['porcentaje_de_palabras_clave'].iloc[indice-1]
+
+    dataset['porcentaje_de_palabras_clave'].replace({"%": ''}, regex=True, inplace=(True))
+    dataset['porcentaje_de_palabras_clave'].replace({",": '.'}, regex=True, inplace=(True))
+
+    dataset['cluster'] = dataset['cluster'].astype(int)
+    dataset['cantidad_de_palabras_clave'] = dataset['cantidad_de_palabras_clave'].astype(int)
+    dataset['porcentaje_de_palabras_clave'] = dataset['porcentaje_de_palabras_clave'].astype(float)
+
+    dataset = dataset.groupby(['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave'], as_index=False)[['principales_palabras_clave']].sum()
+
+    dataset['principales_palabras_clave'].replace({", ": ", "}, regex=True, inplace=(True))
+    dataset['principales_palabras_clave']=dataset['principales_palabras_clave'].str.strip()
+    dataset["principales_palabras_clave"] = dataset["principales_palabras_clave"].str.rstrip('.')
+
+    return dataset
